02c78607d776bc0226112ed6a87d8f06b9c7a8e0
Adrián <asandedominguez@danielcastelao.org>
2026-09-22 10:15:38 +0200

    Primer apartado - primera estructura - mejora

--- src/interfaz.java
+++ src/interfaz.java
@@ -4,11 +4,16 @@ public class interfaz {
     public static void main (String [] args) {
         Scanner numero = new Scanner(System.in);
 
-        while () {
-
+        while (true) {
+            String salir = numero.nextLine();
+            int num = numero.nextInt();
+            if (salir == "salir") {
+                System.out.println("Saliendo del programa");
+                break;
+            }
+            else {
+                System.out.println("Introuduce un número (o 'salir' para terminar): ");
+            }
         }
-
-        System.out.println("Introuduce un número (o 'salir' para terminar): ");
-        int num = numero.nextInt();
     }
 }
