386088ce6dc2abe311095e1d4e6adeedcfd731c7
Adrián <asandedominguez@danielcastelao.org>
2026-09-22 10:10:51 +0200

    Primer apartado - primera estructura

---
+++ src/interfaz.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class interfaz {
+    public static void main (String [] args) {
+        Scanner numero = new Scanner(System.in);
+
+        while () {
+
+        }
+
+        System.out.println("Introuduce un número (o 'salir' para terminar): ");
+        int num = numero.nextInt();
+    }
+}
