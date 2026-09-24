3bcfc99f9de4530aa6276b18f274661c96e00113
Adrián <asandedominguez@danielcastelao.org>
2026-09-24 10:14:45 +0200

    Primer apartado - primera estructura - Primera clase echa

--- src/interfaz.java
+++ src/interfaz.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class interfaz {
     public static void main (String [] args) {
         Scanner numero = new Scanner(System.in);
+        int num;
+        String salir;
 
         while (true) {
-            String salir = numero.nextLine();
-            int num = numero.nextInt();
-            if (salir == "salir") {
+            System.out.println("Introuduce un número (o 'salir' para terminar): ");
+            salir = numero.nextLine();
+            if (salir.equalsIgnoreCase("salir")) {
                 System.out.println("Saliendo del programa");
                 break;
             }
             else {
-                System.out.println("Introuduce un número (o 'salir' para terminar): ");
+                num = Integer.parseInt(salir);
+                System.out.println(num);
             }
         }
     }
