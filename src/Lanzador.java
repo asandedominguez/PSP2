fe8120c48749f1218de833a1036151ec02c839fd
Adrián <asandedominguez@danielcastelao.org>
2026-09-25 14:06:36 +0200

    ejercicio 3 por comenzar

--- src/Lanzador.java
+++ src/Lanzador.java
@@ -26,8 +26,59 @@ public class Lanzador {
         }
 
     }
+
+    public static int factorial2(String numero_introducido) {
+        try {
+            ProcessBuilder comando = new ProcessBuilder("factor", String.valueOf(numero_introducido));
+            Process iniciar = comando.start();
+
+            int decision = iniciar.waitFor();
+            String formateo = (decision == 0) ? "[OK] " : "[ERROR] ";
+
+            BufferedReader lectura = new BufferedReader(new InputStreamReader(iniciar.getInputStream()));
+            String linea;
+            while ((linea = lectura.readLine()) != null) {
+                System.out.println(formateo + linea);
+            }
+            BufferedReader error = new BufferedReader(new InputStreamReader(iniciar.getErrorStream()));
+            while ((error.readLine()) != null) {
+                System.out.println(formateo +" factor: " + numero_introducido + " is not a valid positive integer");
+            }
+            int bien = iniciar.waitFor();
+            System.out.println("Operación completada. Código de salida: " + bien);
+            return bien;
+        }
+        catch (Exception e) {
+            return 1;
+        }
+    }
+
+    public static int factorial3(String numero_introducido) {
+        try {
+            ProcessBuilder comando = new ProcessBuilder("factor", String.valueOf(numero_introducido));
+            Process iniciar = comando.start();
+
+            BufferedReader lectura = new BufferedReader(new InputStreamReader(iniciar.getInputStream()));
+            String linea;
+            while ((linea = lectura.readLine()) != null) {
+                System.out.println(linea);
+            }
+            BufferedReader error = new BufferedReader(new InputStreamReader(iniciar.getErrorStream()));
+            while ((error.readLine()) != null) {
+                System.out.println("factor: " + numero_introducido + " is not a valid positive integer");
+            }
+            int bien = iniciar.waitFor();
+            System.out.println("Operación completada. Código de salida: " + bien);
+            return bien;
+        }
+        catch (Exception e) {
+            return 1;
+        }
+    }
+
+
     public static void main(String[] args) {
         interfaz.interfaz();
-
     }
+
 }
--- src/interfaz.java
+++ src/interfaz.java
@@ -4,17 +4,39 @@ public class interfaz {
     public static void interfaz() {
         Scanner numero = new Scanner(System.in);
         String salir;
+        String nivel = "";
 
-        while (true) {
-            System.out.println("Introuduce un número (o 'salir' para terminar): ");
-            salir = numero.nextLine();
+        while (nivel != "67") {
+            System.out.println("Selecciona el nivel(1,2,3,4) 67 para salir: ");
+            nivel = numero.next();
+            switch (nivel) {
+                case "1":
+                    numero.nextLine();
+                    while (true) {
+                        System.out.println("Introuduce un número (o 'salir' para terminar): ");
+                        salir = numero.nextLine();
 
-            if (salir.equalsIgnoreCase("salir")) {
-                System.out.println("Saliendo del programa");
-                break;
+                        if (salir.equalsIgnoreCase("salir")) {
+                            System.out.println("Saliendo del programa");
+                            break;
+                        }
+                        Lanzador.factorial(salir);
+                    }
+                    break;
+                case "2":
+                    numero.nextLine();
+                    while (true) {
+                        System.out.println("Introuduce un número (o 'salir' para terminar): ");
+                        salir = numero.nextLine();
+
+                        if (salir.equalsIgnoreCase("salir")) {
+                            System.out.println("Saliendo del programa");
+                            break;
+                        }
+                        Lanzador.factorial2(salir);
+                    }
             }
-            Lanzador.factorial(salir);
+        break;
         }
-        numero.close();
     }
 }
