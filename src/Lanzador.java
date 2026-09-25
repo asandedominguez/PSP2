e1e7bb44ba72c8b95881ac526f97c1cb7ee6ba02
Adrián <asandedominguez@danielcastelao.org>
2026-09-25 10:44:48 +0200

    Lanzador actualizado - ejercicio echo

--- src/Lanzador.java
+++ src/Lanzador.java
@@ -1,22 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Lanzador {
     public static int factorial(int numero_introducido) {
-        System.out.print(numero_introducido + ":");
-        // INICIALIZAMOS LA VARIABLE I EN 2, YA QUE ES EL PRIMER NÚMERO CON EL QUE SE PUEDE DIVIDIR
-        // MIENTRAS ESE NÚMERO SEÁ MENOR AL NÚMERO PASADO POR TECLADO SE IRÁ INCREMENTANDO DE 1 EN 1
-        for (int i = 2; i <= numero_introducido; i++) {
+        try {
+            ProcessBuilder comando = new ProcessBuilder("factor", String.valueOf(numero_introducido));
+            Process iniciar = comando.start();
 
-            //SI AL DIVIDIR EL NÚMERO PASADO POR TECLADO ENTRE EL NÚMERO QUE CORRESPONDA EN LA VARABLE I
-            //DA 0, PRINTEARA EN TERMINAL EL NÚMERO QUE A DÍVIDO AL NÚMERO PASADO POR TECLADO
-            while (numero_introducido % i == 0) {
-                System.out.print(" " + i);
-                //ACTUALIZA EL VALOR DE ESTE HASTÁ QUE SE TERMINE LA CONDICIÓN DEL BUCLE FOR
-                numero_introducido /= i;
+            BufferedReader lectura = new BufferedReader(new InputStreamReader(iniciar.getInputStream()));
+            String linea;
+            while ((linea = lectura.readLine()) != null) {
+                System.out.println(linea);
             }
+            return iniciar.waitFor();
         }
-        System.out.println();
-        return 0;
+        catch (Exception e) {
+            return 1;
+        }
+
     }
     public static void main(String[] args) {
         interfaz.interfaz();
